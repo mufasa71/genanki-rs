b88f829e7e5cb46cf5ff2425b27ddf22dc4cd7a8
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2023-03-01 21:18:15 +0100

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Listening on port {}", port);
+    run(listener).expect("Failed to bind address").await
+
 }
+
