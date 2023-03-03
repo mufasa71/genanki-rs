6bbe71c7fe74f1eaaf4b9dfb0c371f55373c42ff
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2023-03-03 10:10:20 +0100

    chore: add db connection

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,14 @@
+pub mod configuration;
+pub mod routes;
+
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use routes::{subscribe, health_check};
 use std::net::TcpListener;
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     HttpResponse::Ok().body(format!("Hello {}!", &name))
-}
-
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::net::TcpListener;
 use zero2prod::run;
+use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    println!("Listening on port {}", port);
-    run(listener).expect("Failed to bind address").await
-
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(&address)?;
+    run(listener)?.await
 }
 
