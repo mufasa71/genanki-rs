19a20de79aed54f663439c18c020f2ba14e07e11
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2024-01-14 14:34:43 +0500

    wip :hammer:

--- goethe-wortliste-b1/src/anki/mod.rs
+++ goethe-wortliste-b1/src/anki/mod.rs
@@ -1,15 +1,11 @@
 use crate::db;
 use base64::decode;
 use genanki_rs::{Deck, Error, Field, Model, Note, Package, Template};
+use indicatif::{ProgressBar, ProgressStyle};
 use std::{fs::read_dir, fs::File, io::Write};
 
-pub fn generate_deck(data: &Vec<(db::WordItem, db::Translation)>) -> Result<(), Error> {
-  let mut my_deck = Deck::new(
-    2059400110,
-    "Goethe-Zertifikat B1",
-    "Deutschprüfung für Jugendliche und Erwachsene",
-  );
-  let my_model = Model::new(
+pub fn basic_model() -> Model {
+  Model::new(
     1607392319,
     "German model (and reversed card)",
     vec![
@@ -29,31 +25,54 @@ pub fn generate_deck(data: &Vec<(db::WordItem, db::Translation)>) -> Result<(), 
       Field::new("Note/Mnemonic"),
       Field::new("CreateReversed"),
     ],
-    vec![
-      Template::new("Card 1")
-        .qfmt(include_str!("template-q.html"))
-        .afmt(include_str!("template-a.html")),
-      Template::new("Card 2")
-        .qfmt(include_str!("template-q-r.html"))
-        .afmt(include_str!("template-a-r.html")),
-    ],
+    vec![Template::new("Card 1")
+      .qfmt(include_str!("template-q.html"))
+      .afmt(include_str!("template-a.html"))],
   )
-  .css(include_str!("minimal.css"));
+  .css(include_str!("minimal.css"))
+}
 
+pub fn basic_model_reversed() -> Model {
+  let model = basic_model();
+  model.with_template(
+    Template::new("Card 2")
+      .qfmt(include_str!("template-q-r.html"))
+      .afmt(include_str!("template-a-r.html")),
+  )
+}
+
+pub fn generate_deck(
+  data: &Vec<(db::WordItem, db::Translation)>,
+  model: &Model,
+) -> Result<(), Error> {
+  let mut my_deck = Deck::new(
+    2059400110,
+    "Goethe-Zertifikat B1",
+    "Deutschprüfung für Jugendliche und Erwachsene",
+  );
+  let bar = ProgressBar::new(data.len() as u64);
+  bar.set_style(
+    ProgressStyle::with_template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+      .unwrap()
+      .progress_chars("##-"),
+  );
   for (word, translation) in data {
+    bar.inc(1);
     match &translation.audio {
       Some(audio) => {
-        let decoded_data = decode(audio).unwrap();
         let file_path = format!("audio/{}.ogg", word.id);
-        let mut file = File::create(&file_path)?;
-        file.write_all(&decoded_data)?;
+        if !std::path::Path::new(&file_path).exists() {
+          let decoded_data = decode(audio).unwrap();
+          let mut file = File::create(&file_path)?;
+          file.write_all(&decoded_data)?;
+        }
       }
       _ => (),
     }
     let description = translation.description.clone().unwrap();
     let word_translation = word.description.clone().unwrap();
     let my_note = Note::new(
-      my_model.clone(),
+      model.clone(),
       vec![
         &format!("[sound:{}.ogg]", word.id),
         &translation.translation,
@@ -74,6 +93,7 @@ pub fn generate_deck(data: &Vec<(db::WordItem, db::Translation)>) -> Result<(), 
     )?;
     my_deck.add_note(my_note);
   }
+  bar.finish();
 
   let mut files = vec![
     "media/_flag_de.svg".to_string(),
@@ -88,5 +108,6 @@ pub fn generate_deck(data: &Vec<(db::WordItem, db::Translation)>) -> Result<(), 
   let files = files.iter().map(String::as_str).collect::<Vec<_>>();
 
   let mut my_package = Package::new(vec![my_deck], files)?;
+
   my_package.write_to_file("goethe-zertifikat-b1.apkg")
 }
--- goethe-wortliste-b1/src/main.rs
+++ goethe-wortliste-b1/src/main.rs
@@ -1,44 +1,102 @@
+use clap::{Arg, ArgAction, Command, Parser};
 use sql_query_builder as sql;
-use std::env;
 use tokio::task::JoinSet;
 
 mod anki;
 mod db;
 mod deepl;
 mod google;
-mod openai;
 mod utils;
+
+#[derive(Parser)]
+struct Cli {
+  command: String,
+  model: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-  let args: Vec<String> = env::args().collect();
-  let command = &args[1];
+  let matches = Command::new("b1")
+    .subcommand_required(true)
+    .arg_required_else_help(true)
+    .subcommand(
+      Command::new("translate")
+        .short_flag('T')
+        .long_flag("translate")
+        .about("Translate words in DB")
+        .arg(
+          Arg::new("speech")
+            .short('s')
+            .long("speech")
+            .help("add text-to-speech")
+            .action(ArgAction::Set)
+            .num_args(1..),
+        ),
+    )
+    .subcommand(
+      Command::new("deck")
+        .short_flag('D')
+        .long_flag("deck")
+        .about("Generate deck")
+        .arg(
+          Arg::new("model")
+            .short('m')
+            .long("model")
+            .help("Anki model")
+            .value_parser(["basic", "basic-reversed"])
+            .action(ArgAction::Set)
+            .require_equals(true)
+            .default_value("basic")
+            .default_missing_value("basic")
+            .num_args(0..=1),
+        ),
+    )
+    .subcommand(
+      Command::new("init")
+        .short_flag('I')
+        .long_flag("init")
+        .about("Initialize DB"),
+    )
+    .subcommand(
+      Command::new("clear")
+        .short_flag('C')
+        .long_flag("clear")
+        .about("Clear DB"),
+    )
+    .subcommand(
+      Command::new("seed")
+        .short_flag('S')
+        .long_flag("seed")
+        .about("Seed DB"),
+    )
+    .get_matches();
 
-  if command == "init" {
-    db::init_db();
+  match matches.subcommand() {
+    Some(("translate", translate_matches)) => {
+      translate_words().await?;
+      if translate_matches.get_flag("speech") {
+        synthesize().await?;
+      }
+    }
+    Some(("deck", deck_matches)) => {
+      let data = db::read_words_and_translations()?;
+      let model = deck_matches
+        .get_one::<String>("model")
+        .map(|s| s.as_str())
+        .expect("Please provide a model");
+
+      if model == "basic" {
+        anki::generate_deck(&data, &anki::basic_model())?;
+      } else if model == "basic-reversed" {
+        anki::generate_deck(&data, &anki::basic_model_reversed())?;
+      }
+    }
+    Some(("init", _)) => db::init_db(),
+    Some(("clear", _)) => db::clear_db()?,
+    Some(("seed", _)) => db::seed_db()?,
+    _ => unreachable!(),
   }
 
-  if command == "clear" {
-    db::clear_db()?;
-  }
-
-  if command == "seed" {
-    db::seed_db()?;
-  }
-
-  if command == "generate_deck" {
-    let data = db::read_words_and_translations()?;
-    anki::generate_deck(&data)?;
-  }
-
-  if command == "translate" {
-    translate_words().await?;
-    synthesize().await?;
-  }
-
-  if command == "generate_image" {
-    openai::image("").await?;
-  }
   Ok(())
 }
 
