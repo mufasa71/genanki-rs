551fbcbb23bd06ceb95558a6e3e9507af6186c1d
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2025-06-21 18:12:27 +0200

    chore: remove check24 and imgur-wallpaper crates

--- check24/src/crawlers/mod.rs
+++
@@ -1,9 +0,0 @@
-mod asaka_crawler;
-mod azia_alliance_crawler;
-mod credit;
-mod text_helpers;
-
-pub use asaka_crawler::*;
-pub use azia_alliance_crawler::*;
-pub use credit::*;
-pub use text_helpers::*;
--- check24/src/main.rs
+++
@@ -1,8 +0,0 @@
-use check24uz::run;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    run().await?;
-
-    Ok(())
-}
