63f67b8958f4bc1963dbc2bfbac346e5e075b977
Shukhrat Mukimov <shukhrat.mukimov@auto1.com>
2025-06-22 22:50:03 +0200

    chore: Remove zero2prod

--- zero2prod/migrations/20250621105719_create_subscriptions_table.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE subscriptions (
-    id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    email text NOT NULL UNIQUE,
-    user_name text NOT NULL,
-    subscribed_at timestamptz NOT NULL DEFAULT now()
-);
--- zero2prod/tests/fixtures/subscriptions.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO subscriptions (
-    email, user_name
-) VALUES ('ursula_le_guin@gmail.com', 'ursula_le_guin');
